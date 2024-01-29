26f98fe97985af765971807dc24e0da4198b8af7
Gareth618 <bloggareth@gmail.com>
2024-01-29 15:02:25 +0200

    third post

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
-import { emojiPlugin, linkPlugin } from './remark/link.mjs'
+import { textPlugin, linkPlugin } from './remark/link.mjs'
 
 export default defineConfig({
   integrations: [mdx(), tailwind(), vue()],
   markdown: {
-    remarkPlugins: [linkPlugin, emojiPlugin],
+    remarkPlugins: [textPlugin, linkPlugin],
   },
 })
