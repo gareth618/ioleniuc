ae2e9f51f15c3257eadbb4572528e0338fdde170
Gareth618 <bloggareth@gmail.com>
2024-07-31 03:51:02 +0300

    removed monospace dates

--- src/components/cards/RecipeCard.vue
+++ src/components/cards/RecipeCard.vue
@@ -21,7 +21,7 @@ const active = ref(new Array(props.ingredients.length).fill(true))
     <div class="flex h-full flex-col justify-between gap-4 bg-gray-700 p-4">
       <div class="grid w-fit grid-cols-[auto,_auto,_auto,_auto] items-center gap-x-4">
         <template v-for="(ingredient, index) in props.ingredients">
-          <button class="aspect-square h-4" :class="active[index] ? 'border border-gray-500' : 'bg-gray-500'" @click="active[index] = !active[index]" />
+          <button class="aspect-square h-4" :class="active[index] ? 'border-2 border-gray-500' : 'bg-gray-500'" @click="active[index] = !active[index]" />
           <div :class="{ 'text-gray-500': !active[index] }" class="text-right font-mono text-sm">{{ Math.round(ingredient[0] * factor * 100) / 100 }}</div>
           <div :class="{ 'text-gray-500': !active[index] }" class="text-center">{{ ingredient[1] }}</div>
           <div :class="{ 'text-gray-500': !active[index] }" class="overflow-hidden whitespace-nowrap">{{ ingredient[2] }}</div>
